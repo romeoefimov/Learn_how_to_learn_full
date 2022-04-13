138aff2fd5f4d73e286b03383b240c7e6b177260
romeoefimov <refimov@edventure.ru>
2022-04-13 20:10:18 +0300

    1

---
+++ story_content/user.js
@@ -0,0 +1,335 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5x9Xj9i8JFP":
+        Script1();
+        break;
+      case "64HPU4mP5fG":
+        Script2();
+        break;
+      case "6R7hiIyrwp8":
+        Script3();
+        break;
+      case "69pyx2EFNCa":
+        Script4();
+        break;
+      case "6UMVSvBLXIV":
+        Script5();
+        break;
+      case "6IPEGzasspJ":
+        Script6();
+        break;
+      case "5xO2Gd88By8":
+        Script7();
+        break;
+      case "5yIYwSB3GBd":
+        Script8();
+        break;
+      case "6OhDoeLyQko":
+        Script9();
+        break;
+      case "6dWAe8nPXr0":
+        Script10();
+        break;
+      case "6jWxqG1S4zA":
+        Script11();
+        break;
+      case "635C66yZS7f":
+        Script12();
+        break;
+      case "6Dh0gbdt4Sr":
+        Script13();
+        break;
+      case "6KmS5PoyyJ1":
+        Script14();
+        break;
+      case "5pUVCRIwQuf":
+        Script15();
+        break;
+      case "6M1Ve2oEjNg":
+        Script16();
+        break;
+      case "5d9MmgBxnJ9":
+        Script17();
+        break;
+      case "5XGzOb3O0wy":
+        Script18();
+        break;
+      case "6KMxNPzlC1R":
+        Script19();
+        break;
+      case "620Cu0DP2s6":
+        Script20();
+        break;
+      case "5co8pam9avX":
+        Script21();
+        break;
+      case "6Bm6llr0Isg":
+        Script22();
+        break;
+      case "6fCU27pB7iG":
+        Script23();
+        break;
+      case "6cSKTFervd7":
+        Script24();
+        break;
+      case "6BbP7MuGox2":
+        Script25();
+        break;
+      case "5cHNKNMvTCk":
+        Script26();
+        break;
+      case "6qUdzOHxs7v":
+        Script27();
+        break;
+      case "6FcFtEZQ2zX":
+        Script28();
+        break;
+      case "5nbDOhYi2qv":
+        Script29();
+        break;
+      case "6oRhx09tvXY":
+        Script30();
+        break;
+      case "6CpVJFavt7w":
+        Script31();
+        break;
+      case "6kEmrlf0zJv":
+        Script32();
+        break;
+      case "6rN28H21lCY":
+        Script33();
+        break;
+      case "6OqAVFUSkK6":
+        Script34();
+        break;
+      case "5XOlCycKjCS":
+        Script35();
+        break;
+      case "5s7zX2bpY7E":
+        Script36();
+        break;
+      case "6Vp9KiV27kt":
+        Script37();
+        break;
+      case "6U41lhjsAxe":
+        Script38();
+        break;
+      case "5kO5lbwG6Ji":
+        Script39();
+        break;
+      case "5j5EoWfbZoq":
+        Script40();
+        break;
+      case "5vxTZWKKLka":
+        Script41();
+        break;
+  }
+}
+
+function Script1()
+{
+  if(void 0===window.stencilfullscreen){window.stencilfullscreen=!0;var player=GetPlayer(),xhttp=new XMLHttpRequest,targlink="https://cluelabs.com/stencil/display/widget-full-screen-display?chart=MzIyfDE3ODB8MWE3MTlmOTlmMmMxOWEyMzExYTE1YTBmNzFmMGI0ZTY";xhttp.onreadystatechange=function(){4==this.readyState&&200==this.status&&""!=this.responseText&&(eval(this.responseText),player.SetVar("stencilrendered",parseInt(player.GetVar("stencilrendered"))+1))},xhttp.open("GET",targlink,!0),xhttp.send()}
+}
+
+function Script2()
+{
+  FullscreenMode('player');
+}
+
+function Script3()
+{
+  FullscreenMode('player');
+}
+
+function Script4()
+{
+  FullscreenMode('player');
+}
+
+function Script5()
+{
+  FullscreenMode('player');
+}
+
+function Script6()
+{
+  FullscreenMode('player');
+}
+
+function Script7()
+{
+  FullscreenMode('player');
+}
+
+function Script8()
+{
+  FullscreenMode('player');
+}
+
+function Script9()
+{
+  FullscreenMode('player');
+}
+
+function Script10()
+{
+  FullscreenMode('player');
+}
+
+function Script11()
+{
+  if(void 0===window.stencilfullscreen){window.stencilfullscreen=!0;var player=GetPlayer(),xhttp=new XMLHttpRequest,targlink="https://cluelabs.com/stencil/display/widget-full-screen-display?chart=MzIyfDE3ODB8MWE3MTlmOTlmMmMxOWEyMzExYTE1YTBmNzFmMGI0ZTY";xhttp.onreadystatechange=function(){4==this.readyState&&200==this.status&&""!=this.responseText&&(eval(this.responseText),player.SetVar("stencilrendered",parseInt(player.GetVar("stencilrendered"))+1))},xhttp.open("GET",targlink,!0),xhttp.send()}
+}
+
+function Script12()
+{
+  FullscreenMode('player');
+}
+
+function Script13()
+{
+  FullscreenMode('player');
+}
+
+function Script14()
+{
+  FullscreenMode('player');
+}
+
+function Script15()
+{
+  FullscreenMode('player');
+}
+
+function Script16()
+{
+  FullscreenMode('player');
+}
+
+function Script17()
+{
+  FullscreenMode('player');
+}
+
+function Script18()
+{
+  FullscreenMode('player');
+}
+
+function Script19()
+{
+  if(void 0===window.stencilfullscreen){window.stencilfullscreen=!0;var player=GetPlayer(),xhttp=new XMLHttpRequest,targlink="https://cluelabs.com/stencil/display/widget-full-screen-display?chart=MzIyfDE3ODB8MWE3MTlmOTlmMmMxOWEyMzExYTE1YTBmNzFmMGI0ZTY";xhttp.onreadystatechange=function(){4==this.readyState&&200==this.status&&""!=this.responseText&&(eval(this.responseText),player.SetVar("stencilrendered",parseInt(player.GetVar("stencilrendered"))+1))},xhttp.open("GET",targlink,!0),xhttp.send()}
+}
+
+function Script20()
+{
+  FullscreenMode('player');
+}
+
+function Script21()
+{
+  FullscreenMode('player');
+}
+
+function Script22()
+{
+  FullscreenMode('player');
+}
+
+function Script23()
+{
+  FullscreenMode('player');
+}
+
+function Script24()
+{
+  FullscreenMode('player');
+}
+
+function Script25()
+{
+  FullscreenMode('player');
+}
+
+function Script26()
+{
+  FullscreenMode('player');
+}
+
+function Script27()
+{
+  FullscreenMode('player');
+}
+
+function Script28()
+{
+  FullscreenMode('player');
+}
+
+function Script29()
+{
+  FullscreenMode('player');
+}
+
+function Script30()
+{
+  FullscreenMode('player');
+}
+
+function Script31()
+{
+  FullscreenMode('player');
+}
+
+function Script32()
+{
+  if(void 0===window.stencilfullscreen){window.stencilfullscreen=!0;var player=GetPlayer(),xhttp=new XMLHttpRequest,targlink="https://cluelabs.com/stencil/display/widget-full-screen-display?chart=MzIyfDE3ODB8MWE3MTlmOTlmMmMxOWEyMzExYTE1YTBmNzFmMGI0ZTY";xhttp.onreadystatechange=function(){4==this.readyState&&200==this.status&&""!=this.responseText&&(eval(this.responseText),player.SetVar("stencilrendered",parseInt(player.GetVar("stencilrendered"))+1))},xhttp.open("GET",targlink,!0),xhttp.send()}
+}
+
+function Script33()
+{
+  FullscreenMode('player');
+}
+
+function Script34()
+{
+  FullscreenMode('player');
+}
+
+function Script35()
+{
+  FullscreenMode('player');
+}
+
+function Script36()
+{
+  FullscreenMode('player');
+}
+
+function Script37()
+{
+  FullscreenMode('player');
+}
+
+function Script38()
+{
+  FullscreenMode('player');
+}
+
+function Script39()
+{
+  FullscreenMode('player');
+}
+
+function Script40()
+{
+  FullscreenMode('player');
+}
+
+function Script41()
+{
+  FullscreenMode('player');
+}
+
